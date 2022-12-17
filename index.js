f4f266a4c2c1b7a83535d083385958c43f1a1461
dimaboi2101 <dimaboi21@gmail.com>
2022-12-17 20:29:48 +0300

    portfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.menu-btn')
+const menuList = document.querySelector('.nav__list')
+
+menuBtn.addEventListener("click" , () => {
+    menuList.classList.toggle("nav__active")
+})
+menuBtn.addEventListener("click" , () => {
+    menuList.classList.toggle("nav__active")
+})
